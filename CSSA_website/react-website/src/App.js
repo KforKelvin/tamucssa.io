268fe4f92e9fab2cbee7dcabfd53d47851a9ce5e
Kelvin Gao <ky1015@tamu.edu>
2022-07-03 15:59:47 -0500

    first upload

---
+++ CSSA_website/react-website/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+// import Navbar from './components/Navbar';
+// import Home from './components/pages/Home';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import Services from './components/pages/Services';
+// import Products from './components/pages/Products';
+// import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome</h1>
+    </div>
+  );
+}
+
+export default App;

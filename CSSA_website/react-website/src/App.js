56562ccec60a71460e5584b1d6639dadc3a7491b
Kelvin Gao <ky1015@tamu.edu>
2022-07-04 14:59:06 -0500

    update #2

--- CSSA_website/react-website/src/App.js
+++ CSSA_website/react-website/src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import './App.css';
-// import Navbar from './components/Navbar';
-// import Home from './components/pages/Home';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCab} from '@fortawesome/free-solid-svg-icons'
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Services from './components/pages/Services';
 // import Products from './components/pages/Products';
 // import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome</h1>
-    </div>
+    
+    <>
+    <Router>
+    <h1>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact components={Home}/>
+        </Routes>
+        {/* <FontAwesomeIcon icon={faCab}></FontAwesomeIcon> */}
+      </h1>
+    </Router>
+   
+     
+    </>
+      
+    
   );
 }
 
